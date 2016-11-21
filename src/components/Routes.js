75caeea860faf7ebd99e71da04cffd93db7dc04c
Brian Jacobel <bjacobel@gmail.com>
2016-11-20 20:29:07 -0500

    Start setting up google analytics

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Match, Miss, BrowserRouter } from 'react-router';
+import ReactGA from 'react-ga';
 
 import NotFound from './NotFound';
 import DogPage from './DogPage';
 import Main from './Main';
 
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 export default class Routes extends Component {
+  componentWillMount() {
+    ReactGA.initialize('UA-34138258-6');
+  }
+
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter onUpdate={ logPageView }>
         <div>
           <Match pattern="/" exactly component={ Main } />
           <Match pattern="/dog/:id" component={ DogPage } />
